daf10ed76e25ebf900eabe618c2042d8f54f99e4
wanwalee <wanwalew@gmail.com>
2018-06-01 04:21:29 +0000

    start app

---
+++ Onewa/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Onewa
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://onewa.azurewebsites.net";
+	}
+}
+
